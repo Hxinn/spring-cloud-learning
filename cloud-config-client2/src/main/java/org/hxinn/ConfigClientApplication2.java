e437997b008bc7d0ca57934696dc96a70db59b0c
Hxinn <445623551@qq.com>
2021-06-05 23:58:52 +0800

    update: bus
    集成RabbitMQ使用消息总线来推送配置
    调用配置中的actuator/bus-refresh 将配置文件推送到客户端
    使用 actuator/bus-refresh/[服务名字]:[端口] 实现定点推送

---
+++ cloud-config-client2/src/main/java/org/hxinn/ConfigClientApplication2.java
@@ -0,0 +1,12 @@
+package org.hxinn;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication2 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication2.class,args);
+    }
+}
